6e1e0308e49b5fb688aefaaa17b3980a6a9ed749
aldair-meneses <aldairtecn@hotmail.com>
2021-12-23 03:11:12 -0300

    second commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const A = document.getElementById("a")
+const B = document.getElementById("b")
+const C = document.getElementById("c")
+const $X1 = document.getElementById("X1")
+const $X2 = document.getElementById("X2")
+const calc = document.querySelector('button');
+
+function bhaskara() {
+    const a = A.value;
+    const b = B.value;
+    const c = C.value;
+    
+    const delta = b ** 2 - 4 * a * c;
+
+    const X1 = (-b + Math.sqrt(delta)) / (2 * a);
+    const X2 = (-b - Math.sqrt(delta)) / (2 * a);   
+    $X1.value = X1
+    $X2.value = X2
+    
+}
+calc.addEventListener('click', function () {
+    bhaskara();  
+}); 
