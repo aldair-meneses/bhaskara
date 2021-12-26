0b8cbdcc6b114411f4fc7638d7831bfacd813774
aldair-meneses <aldairtecn@hotmail.com>
2021-12-26 02:12:17 -0300

    7th commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const A = document.getElementById("a")
 const B = document.getElementById("b")
 const C = document.getElementById("c")
-const $X1 = document.getElementById("X1")
-const $X2 = document.getElementById("X2")
-const calc = document.querySelector('button');
+const x1 = document.getElementById("X1")
+const x2 = document.getElementById("X2")
+const calc = document.querySelector("button");
 
 function bhaskara() {
     const a = A.value;
@@ -11,13 +11,21 @@ function bhaskara() {
     const c = C.value;
 
     const delta = b ** 2 - 4 * a * c;
+    
+    if (a == '' || b == '' || c == '') {
+        document.getElementById("xnr").innerHTML = ("Digite os valores de A, B e C");
+    }else if (delta < 0) {
+        document.getElementById('xnr').innerHTML = ("Não possui raizes reais.")
+    }else if(a == 0) {
+        document.getElementById("xnr").innerHTML = ("O valor de A não pode ser 0");
+    } else {  
+        const X1 = (-b + Math.sqrt(delta)) / (2 * a);
+        const X2 = (-b - Math.sqrt(delta)) / (2 * a);
+        x1.value = X1;
+        x2.value = X2;
+    }
+}   
 
-    const X1 = (-b + Math.sqrt(delta)) / (2 * a);
-    const X2 = (-b - Math.sqrt(delta)) / (2 * a);   
-    $X1.value = X1
-    $X2.value = X2
-// não possui resultados reais 
-}
 
 calc.addEventListener('click', function () {    
     bhaskara();
