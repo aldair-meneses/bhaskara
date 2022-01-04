ac74a24d1217daa94bd5b32683ac76d0dccd2851
aldair-meneses <aldairtecn@hotmail.com>
2022-01-03 21:45:51 -0300

    operator fix

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const A = document.getElementById('a')
-const B = document.getElementById('b')
-const C = document.getElementById("c")
-const x1 = document.getElementById('X1')
-const x2 = document.getElementById('X2')
-const calc = document.querySelector('button');
+const A = document.getElementById('a');
+const B = document.getElementById('b');
+const C = document.getElementById('c');
+const x1 = document.getElementById('X1');
+const x2 = document.getElementById('X2');
+const calc = document.querySelector('button')
 
 function bhaskara() {
     const a = A.value;
     const b = B.value;
     const c = C.value;
+
+
 
     const delta = b ** 2 - 4 * a * c;
     
@@ -18,30 +20,36 @@ function bhaskara() {
         createMsg("Não possui raizes reais.");
     }else if(a == 0) {
         createMsg("O valor de A não pode ser 0.");
-    } else {  
+    }else {  
         const X1 = (-b + Math.sqrt(delta)) / (2 * a);
         const X2 = (-b - Math.sqrt(delta)) / (2 * a);
-        x1.value = X1;
+        x1.value = X1;          
         x2.value = X2;
     }
-}   
+
+    if (x1.value == 'NaN'){
+        createMsg("\nCaracter inválido.\n")
+    }
+    
+}
+
+
+
 function createMsg(txt) {
     document.getElementById('warnings').innerHTML = (`<h1 id="temp">${txt}</h1>`);
     setTimeout ( function () {
         removeMsg();
-    }, 2500);
+    }, 3000);
    
-}
+};
+
+
 
 function removeMsg() {
-    const text = document.getElementById("temp")
+    const text = document.getElementById("temp");
+    const numbers = x1.value
     text.remove();
-}
-function isNumberKey(evt){
-    var charCode = (evt.which) ? evt.which : evt.keyCode
-    return !(charCode > 31 && (charCode < 48 || charCode > 57));
-}
-
+};
 
 calc.addEventListener('click', function() {
     bhaskara();
