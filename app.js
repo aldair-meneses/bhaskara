313875aa67249eedadb625f3e9f648f366a4733d
Aldair Meneses <aldrick@pop-os.localdomain>
2022-01-27 16:01:26 -0300

    fix 27/01

--- app.js
+++ app.js
@@ -6,48 +6,55 @@ const x2 = document.getElementById('X2');
 const calc = document.querySelector('button')
 
 function bhaskara() {
-    const a = A.value;
-    const b = B.value;
-    const c = C.value;
+    const a = parseInt(A.value);
+    const b = parseInt(B.value);
+    const c = parseInt(C.value);
 
     const delta = b ** 2 - 4 * a * c;
     
     if (a == '' || b == '' || c == '') {
         createMsg("Digite os valores de A, B e C.");
     }else if (delta < 0) {
-        createMsg("Não possui raizes reais.");
+        createMsg("Não possui raízes reais.");
     }else if(a == 0) {
         createMsg("O valor de A não pode ser 0.");
     }else {  
         const X1 = (-b + Math.sqrt(delta)) / (2 * a);
         const X2 = (-b - Math.sqrt(delta)) / (2 * a);
-        x1.value = X1;          
-        x2.value = X2;
-    }
-
-    if (x1.value == 'NaN' || x2.value == 'NaN'){
-        createMsg("\nCaracter inválido.\n")
+        x1.value = X1;
+        x2.value = X2;   
     }
     
+    const letras = /[a-zA-Z]/g;
+    if (letras.test(a) == true){
+        createMsg("\nO Caractere digitado para A é inválido");
+    } 
+    else if (letras.test(b) == true){
+        createMsg("\nO Caractere digitado para B é inválido");
+    }
+    else if (letras.test(c) == true){
+        createMsg("\nO Caractere digitado para C é inválido");
+    }
+    x1.value = '';
+    x2.value = '';
 }
+
 
 function createMsg(txt) {
     document.getElementById('warnings').innerHTML = (`<h1 id="temp">${txt}</h1>`);
     setTimeout ( function () {
         removeMsg();
     }, 3000);
-   
-};
+   };
 
 
 
 function removeMsg() {
     const text = document.getElementById("temp");
-    const numbers = x1.value
     text.remove();
 };
 
-calc.addEventListener('click', function() {
-    bhaskara();
+calc.addEventListener('click', function(event) {
+    event.preventDefault(),bhaskara();
 }); 
 
