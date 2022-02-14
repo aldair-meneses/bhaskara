2ec528a4cd8a8578012b9f8b87e716dc92675a48
aldair-meneses <aldairtecn@hotmail.com>
2022-02-14 16:34:43 -0300

    new design bhaskara

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+const guide_button = document.querySelector('#guide_btn')
+const guide = document.querySelector('#guide_page')
+const close = document.querySelector('#close_btn')
+const modal = document.querySelector('#modal_content')
+
+
+function modal_show() {
+    modal.style.display = "block"    
+}
+
+guide_button.addEventListener("click", modal_show )
+
+function modal_close() {
+    modal.style.display = "none"
+}
+
+close.addEventListener("click", modal_close)
+
+
 const A  = document.getElementById("a");
 const B = document.getElementById("b");
 const C = document.getElementById("c");
