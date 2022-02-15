e05b6b46be04a5fd700b4877d3cb323495f244c2
aldair-meneses <aldairtecn@hotmail.com>
2022-02-15 18:51:17 -0300

    new design 1.4

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const guide_button = document.getElementById('guide_btn')
-// const guide = document.querySelector('#guide_page')
-const modal = document.getElementById('modal_content')
-const close = document.getElementById('close_btn')
-guide_button.addEventListener('click', ()=> {
-    modal.classList.add('show');
+const guide_button = document.querySelector('#guide_btn');
+const guide = document.querySelector('#modal_content');
+const close_guide = document.querySelector('#close_btn');
+const container = document.querySelector('#head_container')
+
+
+guide_button.addEventListener('click', (event)=> {
+    guide.classList.add('show');
+    if (event.target == guide){
+        guide.classList.remove('show')
+    }
 })
 
-close.addEventListener('click', ()=>{
-    modal.classList.remove('show')
+close_guide.addEventListener('click', ()=>{
+    guide.classList.remove('show');
 })
 
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
+
+// guide.addEventListener('click', function(event){
+//     if (event.target == guide) {
+//         guide.classList.remove('show');
 //     }
-// }
+// })
 
 
 
