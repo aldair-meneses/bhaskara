313ccb0399e2945b79294e6a978a56b4314734a2
aldair-meneses <aldairtecn@hotmail.com>
2022-02-15 05:23:31 -0300

    new design

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-const guide_button = document.querySelector('#guide_btn')
-const guide = document.querySelector('#guide_page')
-const close = document.querySelector('#close_btn')
-const modal = document.querySelector('#modal_content')
+const guide_button = document.getElementById('guide_btn')
+// const guide = document.querySelector('#guide_page')
+const modal = document.getElementById('modal_content')
+const close = document.getElementById('close_btn')
+guide_button.addEventListener('click', ()=> {
+    modal.classList.add('show');
+})
+
+close.addEventListener('click', ()=>{
+    modal.classList.remove('show')
+})
+
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
 
 
-function modal_show() {
-    modal.style.display = "block"    
-}
-
-guide_button.addEventListener("click", modal_show )
-
-function modal_close() {
-    modal.style.display = "none"
-}
-
-close.addEventListener("click", modal_close)
 
 
-const A  = document.getElementById("a");
-const B = document.getElementById("b");
-const C = document.getElementById("c");
+
+
+
+
+
+
+// const A  = document.getElementById("a");
+// const B = document.getElementById("b");
+// const C = document.getElementById("c");
