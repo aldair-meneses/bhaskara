d62f127d3c2ee94709fa41ab6c90f8f177014c38
aldair-meneses <aldairtecn@hotmail.com>
2022-02-18 05:58:18 -0300

    add equations

--- script.js
+++ script.js
@@ -17,27 +17,27 @@ guide.addEventListener('click', function(event){
     }
 })
 
-const a = document.querySelector('#a')
-const b = document.querySelector('#b')
-const c = document.querySelector('#c')
+const ax = document.querySelector('#a')
+const bx = document.querySelector('#b')
+const C = document.querySelector('#c')
 const x1 = document.querySelector('#x1')
 const x2 = document.querySelector('#x2')
 const calc = document.querySelector('#calc')
 
 function bhaskara(){
-    const $a = a.value;
-    const $b = b.value;
-    const $c = c.value;
+    const a = ax.value;
+    const b = bx.value;
+    const c = C.value;
 
-    const delta = $b * $b - 4 * $a * $c;
+    const delta = b * b - 4 * a * c;
 
-    const $x1 = (-$b + Math.sqrt(delta)) / (2 * $a);
-    const $x2 = (- $b - Math.sqrt(delta)) / (2 * $a);
+    const $x1 = (-b + Math.sqrt(delta)) / (2 * a);
+    const $x2 = (- b - Math.sqrt(delta)) / (2 * a);
     x1.value = $x1;
     x2.value = $x2;
 
-  // print_equation('\\[ x = \\frac{-b \\pm \\sqrt{b^{2} - 4ac} }{2a} \\]')    
-  print_equation(`\\[ ${$a}x² + ${$b}x + ${$c} = 0 \\]`)
+  print_equation(`\\[ x = \\frac{-${b} \\pm \\sqrt{${b}^{2} - 4\\cdot (${a}\\cdot ${c})} }{2\\cdot (${a}) } \\]`)    
+//   print_equation(`\\[ ${a}x² + ${b}x + ${$c} = 0 \\]`)
 
     
 }
