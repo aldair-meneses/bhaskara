28912349fc74c2246bfc50b0af73386a5575bff7
aldair-meneses <aldairtecn@hotmail.com>
2022-03-02 08:42:45 -0300

    format functions

--- main.js
+++ main.js
@@ -1,44 +1,55 @@
-import {print_equation} from "./module.js";
+import {print_equation, guides, drag} from "./module.js";
+import {window_result} from "./module.js";
 
-globalThis.addEventListener('load', ()=> {
-    const drag =  document.getElementById('drag_off');
-    drag.draggable = false;
-});    
 
-const close_guide = document.querySelector('#close_btn');
-const guide_button = document.querySelector('#guide_btn');
-const help = document.querySelector('#guide_container');
+const close_guide = document.querySelector("#close_btn");
+const guide_button = document.querySelector("#guide_btn");
+const help = document.querySelector("#guide_container");
 
-guide_button.addEventListener('click', ()=> {
-    help.classList.add('show');
+guide_button.addEventListener("click", ()=> {
+    help.classList.add("show");
 });
 
-help.addEventListener('click', (event)=>{
+help.addEventListener("click", (event)=>{
     if (event.target == help) {
-        help.classList.remove('show');
+        help.classList.remove("show");
     }
 });
 
-close_guide.addEventListener('click', ()=>{
-    help.classList.remove('show')
+close_guide.addEventListener("click", ()=>{
+    help.classList.remove("show");
 });
 
-const ax = document.querySelector('#a');
-const bx = document.querySelector('#b');
-const c_ = document.querySelector('#c');
-const calc = document.querySelector('#calc');
-const window_txt = document.querySelector('#window_result');
+const ax = document.querySelector("#a");
+const bx = document.querySelector("#b");
+const c_ = document.querySelector("#c");
+const calc = document.querySelector("#calc");
+
+
+
 
 function bhaskara(){
+
     const a = ax.value;
     const b = bx.value;
     const c = c_.value;
     const delta = b * b - 4 * a * c;
+
     
-    print_equation('funciou amigo', 'error')
+    print_equation("funciou amigo", "error");
+
+    if (delta <  0) {
+        guides()
+        window_result.remove()
+        print_equation("\\[x =  {-b \\pm \\sqrt { b^2 -4 \\cdot a \\cdot c } \\over 2 \\cdot a}  \\]","formula")
+    }
+    MathJax.typeset()
+
 };
 
-calc.addEventListener('click', (event)=>{
+
+globalThis.addEventListener("load", (event)=>{
+    drag();
     event.preventDefault();
     bhaskara();
 });
--- module.js
+++ module.js
@@ -1,12 +1,25 @@
-export {print_equation}
+export {print_equation, guides, drag, window_result}
+
 
 function print_equation(equation, id) {
-    let math = document.getElementById(`${id}`)
-    math.innerHTML = (equation)
+    let math = document.getElementById(`${id}`);
+    math.innerHTML = (equation);
 }
 
+function guides(){
+    let result_guide = document.querySelectorAll(".half");
+    for (guides of result_guide){
+        guides.style.display = ("block")
+    }
+}
 
+const window_result = document.querySelector("#window_result");
 
+function drag(){
+    const drag =  document.getElementById("drag_off");
+    drag.draggable = false;
+}
 
+    
 
 
