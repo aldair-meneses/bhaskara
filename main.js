3af6feaa17b6877232aa33985418b865313394e8
aldair-meneses <aldairtecn@hotmail.com>
2022-03-03 16:07:16 -0300

    *

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import {guides, drag, print_equation} from "./module.js";
+import {how_to, drag, print_equation, non_root,full_sub} from "./module.js";
 import {window_result} from "./module.js";
 
 const close_guide = document.querySelector("#close_btn");
 const guide_button = document.querySelector("#guide_btn");
 const help = document.querySelector("#guide_container");
+
+
 
 guide_button.addEventListener("click", ()=> {
     help.classList.add("show");
@@ -29,26 +31,35 @@ function bhaskara(){
     const a = ax.value;
     const b = bx.value;
     const c = c_.value;
+    
     const delta = b * b - 4 * a * c;
 
-    print_equation("funciou amigo", "error");
+    print_equation("funcionou amigo", "error");
 
     if (delta <  0) {
-        guides()
-        window_result.remove()
+        non_root()
+        how_to();
+        window_result.remove();
+    }else {
+        full_sub();
+        const x1 = (-b + Math.sqrt(delta)) / 2 * a;
+        const x2 = (-b - Math.sqrt(delta)) / 2 * a;
         print_equation("\\[x =  {-b \\pm \\sqrt { b^2 -4 \\cdot a \\cdot c } \\over 2 \\cdot a}  \\]","formula");
         print_equation("\\[ax^2 + bx + c = 0 \\]", "equation_ex");
         print_equation(`\\[ a = ${a} \\quad b = ${b} \\quad  c = ${c} \\]`, "add_equation");
-        print_equation("\\[ \\Delta = {b^2 - 4 \\cdot a \\cdot c }\\]", "delta_ex")
-        print_equation(`\\[ \\Delta = {${b}^2 - 4 \\cdot ${a} \\cdot ${c}} \\]`,"add_delta")
-        print_equation(`\\[ \\Delta = ${delta}\\]`,"delta_solved")
+        print_equation("\\[ \\Delta = {b^2 - 4 \\cdot a \\cdot c }\\]", "delta_ex");
+        print_equation(`\\[ \\Delta = {${b}^2 - 4 \\cdot ${a} \\cdot ${c}} \\]`,"add_delta");
+        print_equation(`\\[ \\Delta = ${delta}\\]`,"delta_solved");
+        print_equation('\\[ x = {-b \\pm \\sqrt {\\Delta} \\over2 \\cdot a} \\]', 'add_formula_ex')
+        print_equation(`\\[ x = { - ${b} \\pm \\sqrt  {${delta}} \\ \\over 2 \\cdot (${a})}  \\]`, 'add_formula');
+        // print_equation(`\\[ x' = ${x1} \\]`, 'x_1');
+        // print_equation(`\\[ x'' = ${x2} \\]`, 'x_2');
     }
-    MathJax.typeset()
 
+    MathJax.typeset();
 };
 
-globalThis.addEventListener("load", (event)=>{
+globalThis.addEventListener("click", ()=>{
     drag();
-    event.preventDefault();
     bhaskara();
 });
--- module.js
+++ module.js
@@ -1,14 +1,14 @@
-export {guides, drag, print_equation, window_result}
+export {how_to, drag, print_equation, non_root, full_sub, window_result}
 
 function print_equation(equation, id) {
     const math = document.getElementById(`${id}`);
     math.innerHTML = (equation);
 }
 
-function guides(){
-    let result_guide = document.querySelectorAll(".half");
-    for (guides of result_guide){
-        guides.style.display = ("block")
+function how_to(how){
+    let how_to_half = document.querySelectorAll('.half');
+    for (how of how_to_half){
+        how.style.display = ("block");
     }
 }
 
@@ -19,6 +19,18 @@ function drag(){
     drag.draggable = false;
 }
 
-    
+function non_root(){
+    let delta = b.value **2 - 4 * a.value * c.value;
+    print_equation("\\[x =  {-b \\pm \\sqrt { b^2 -4 \\cdot a \\cdot c } \\over 2 \\cdot a}  \\]","formula");
+    print_equation("\\[ax^2 + bx + c = 0 \\]", "equation_ex");
+    print_equation(`\\[ a = ${a.value} \\quad b = ${b.value} \\quad  c = ${c.value} \\]`, "add_equation");
+    print_equation("\\[ \\Delta = {b^2 - 4 \\cdot a \\cdot c }\\]", "delta_ex")
+    print_equation(`\\[ \\Delta = {${b.value}^2 - 4 \\cdot ${a.value} \\cdot ${c.value}} \\]`,"add_delta")
+    print_equation(`\\[ \\Delta = ${delta}\\]`,"delta_solved");
+}
 
-
+function full_sub(){
+    how_to();
+    var full_bhaskara = document.getElementById("result_container") 
+    full_bhaskara.insertAdjacentElement("afterend", "<p></p>");
+}
